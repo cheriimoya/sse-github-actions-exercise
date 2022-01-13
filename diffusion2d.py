fe6d55fe98d5264c12659662b603f32072345a6e
Max Hausch <github@hausch.xyz>
2022-01-13 16:57:16 +0100

    Assert input parameters are floats

--- diffusion2d.py
+++ diffusion2d.py
@@ -38,6 +38,12 @@ class SolveDiffusion2D:
         self.dt = None
 
     def initialize_domain(self, w=10., h=10., dx=0.1, dy=0.1):
+        # Check if the parameters are floats
+        assert isinstance(w, float), 'w must be a float'
+        assert isinstance(h, float), 'h must be a float'
+        assert isinstance(dx, float), 'dx must be a float'
+        assert isinstance(dy, float), 'dy must be a float'
+
         self.w = w
         self.h = h
         self.dx = dx
@@ -45,7 +51,11 @@ class SolveDiffusion2D:
         self.nx = int(w / dx)
         self.ny = int(h / dy)
 
-    def initialize_physical_parameters(self, d=4., T_cold=300, T_hot=700):
+    def initialize_physical_parameters(self, d=4., T_cold=300., T_hot=700.):
+        assert isinstance(d, float), 'd must be a float'
+        assert isinstance(T_cold, float), 'T_cold must be a float'
+        assert isinstance(T_hot, float), 'T_hot must be a float'
+
         self.D = d
         self.T_cold = T_cold
         self.T_hot = T_hot
